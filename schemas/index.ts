7e042db95e1786508514645a51567920b4bd5daa
Krishna Lair <krishnaclosalair@gmail.com>
2023-02-28 14:48:17 +0000

    Add page schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,6 @@
-import {product} from "./product";
-import {category} from "./category";
-import {post} from "./post";
+import {product} from './product'
+import {category} from './category'
+import {post} from './post'
+import {page} from './page'
 
-export const schemaTypes = [product, category, post];
+export const schemaTypes = [product, category, post, page]
---
+++ schemas/page.ts
@@ -0,0 +1,36 @@
+import { defineType, defineField } from 'sanity'
+import { validation } from "./utils/validation";
+
+const { required } = validation;
+
+export const page = defineType({
+  name: "page",
+  title: "Page",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: required,
+    }),
+    defineField({
+      name: "copy",
+      title: "Copy",
+      type: "array",
+      of: [{ type: "block" }],
+    }),
+    defineField({
+      name: "image",
+      title: "Image",
+      type: "image",
+      fields: [
+        {
+          name: "alt",
+          title: "Alt",
+          type: "string",
+        },
+      ],
+    }),
+  ],
+});
