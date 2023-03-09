feab16246d34c78f1b965d9db023b6daa54e74f2
Krishna Lair <krishnaclosalair@gmail.com>
2023-03-09 16:08:53 +0000

    Add images on portable text block

--- schemas/post.ts
+++ schemas/post.ts
@@ -1,76 +1,78 @@
-import { defineType, defineField } from 'sanity'
-import { validation } from "./utils/validation";
+import {defineType, defineField} from 'sanity'
+import {validation} from './utils/validation'
 
-const { required } = validation;
+const {required} = validation
 
 export const post = defineType({
-  name: "post",
-  title: "Post",
-  type: "document",
+  name: 'post',
+  title: 'Post',
+  type: 'document',
   fields: [
     defineField({
-      name: "date",
-      title: "Date",
-      type: "date",
+      name: 'date',
+      title: 'Date',
+      type: 'date',
       options: {
         dateFormat: 'YYYY-MM-DD',
       },
-      placeholder: `${new Date().getFullYear()}-${`00${(new Date().getMonth() + 1)}`.slice(-2)}-${new Date().getDate()}`,
+      placeholder: `${new Date().getFullYear()}-${`00${new Date().getMonth() + 1}`.slice(
+        -2
+      )}-${new Date().getDate()}`,
       validation: required,
     }),
     defineField({
-      name: "title",
-      title: "Title",
-      type: "string",
+      name: 'title',
+      title: 'Title',
+      type: 'string',
       validation: required,
     }),
     defineField({
-      name: "copy",
-      title: "Copy",
-      type: "array",
-      of: [{ type: "block" }],
+      name: 'copy',
+      title: 'Copy',
+      type: 'array',
+      of: [{type: 'block'}, {type: 'image'}],
     }),
     defineField({
-      name: "exerpt",
-      title: "Excerpt",
-      type: "array",
-      of: [{ type: "block" }],
+      name: 'exerpt',
+      title: 'Excerpt',
+      type: 'array',
+      of: [{type: 'block'}],
     }),
     defineField({
-      name: "image",
-      title: "Image",
-      type: "image",
+      name: 'image',
+      title: 'Image',
+      type: 'image',
       fields: [
         {
-          name: "alt",
-          title: "Alt",
-          type: "string",
+          name: 'alt',
+          title: 'Alt',
+          type: 'string',
         },
       ],
     }),
     defineField({
-      name: "category",
-      title: "Category",
-      type: "array",
+      name: 'category',
+      title: 'Category',
+      type: 'array',
       of: [
         {
-          type: "reference",
+          type: 'reference',
           to: {
-            type: "category",
+            type: 'category',
           },
         },
       ],
       validation: required,
     }),
     defineField({
-      name: "slug",
-      title: "Slug",
-      type: "slug",
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
       options: {
-        source: "name",
+        source: 'name',
         maxLength: 96,
       },
       validation: required,
     }),
   ],
-});
+})
